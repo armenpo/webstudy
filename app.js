a3256a94e979d3a25ad2e97bbcb5a8ff2a892bd6
armenpo <armenpo@icloud.com>
2020-02-13 19:37:53 +0900

    routing description, server request,response / next MVC patterns

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ import express from "express";
 
 const app = express();
 
+const handleHome = (req, res) => {
+  res.send("HOME!");
+};
+
+const handleLogin = (req, res) => {
+  res.send("Login");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 export default app;
