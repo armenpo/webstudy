fef697ff14160760878aba641637f91ed66bd92f
armenpo <armenpo@icloud.com>
2020-02-14 15:45:05 +0900

    middlewares and custom middleWares

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 import express from "express";
 import homeRouter from "./router/homeRouter";
 import routes from "./routes";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { helloMiddleWare } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// custom middleWare
+app.use(helloMiddleWare);
 app.use(routes.home, homeRouter);
 
 export default app;
